f0764f50d78157b9fdd4d426448910194d0f56da
Anna Tran <annatran@annas-air.mynet>
2020-07-17 15:32:28 +0100

    display shopping list

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const server = express();
 const port = 3000;
 
-server.use(express());
 server.use(cors());
 server.use(bodyParser.text());
 
@@ -20,18 +19,23 @@ const shoppingList = [
     }
 ];
 
-server.listen(port, () => console.log(`This is the server: http://localhost:${port}`));
-
 server.get('/', (req, res) => {
     res.send('Hello Client!')
 })
 
 server.get('/shoppingList', (req, res) => {
-    res.send(JSON.stringify(shoppingList));
+    res.send(JSON.stringify({ shoppingList })); // Sends data that we have on shopping list
 })
 
 server.post('/shoppingList', (req, res) => {
-    const newItem = JSON.parse(req.body);
+    const newItem = JSON.parse(req.body); // Get the request body & turn it into JSON
     shoppingList.push(newItem);
     res.send(JSON.stringify(newItem))
 })
+
+server.delete('/shoppingList/:id', (req, res) => { 
+    // newItem = JSON.parse(req.body);
+    // shoppingList.pop(newItem);
+})
+
+server.listen(port, () => console.log(`This is the server: http://localhost:${port}`));
