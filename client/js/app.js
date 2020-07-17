2cc9e12825e1f779db2343b16993a2ab00998bfb
Anna Tran <annatran@annas-air.mynet>
2020-07-17 15:33:31 +0100

    add button function

--- client/js/app.js
+++ client/js/app.js
@@ -1,14 +1,18 @@
 const form = document.querySelector('#shopping-list-form');
 const shoppingList = document.querySelector('ul');
+// const btn = document.querySelector('#delete');
 
 form.addEventListener('submit', submitList);
+// btn.addEventListener('click', deleteItem);
+
+// Display all the items that has been added by the user
 
 getAllItems();
 
 function getAllItems(){
     fetch('http://localhost:3000/shoppingList')
         .then(r => r.json())
-        .then(appendItems)
+        .then(appendAllItems)
         .catch(console.warn)
 }
 
@@ -26,13 +30,14 @@ function submitList(e) {
     };
 
     fetch('http://localhost:3000/shoppingList', options)
-        .then(r => r.json())
-        .then(appendItem)
+        .then(r => r.json()) // As this is a HTTP response, we want to extract JSON body content from the response
+        .then(appendItem) 
         .catch(console.warn)
 };
 
-function appendItems(data){
-    data.shopItems.forEach(appendItem);
+function appendAllItems(data){
+    data.shoppingList.forEach(appendItem); 
+    console.log(data.shoppingList);
 };
 
 function appendItem(shoppingData) {
@@ -40,3 +45,27 @@ function appendItem(shoppingData) {
     newLi.textContent = `Item: ${shoppingData.item}, Price £${shoppingData.price}`
     shoppingList.append(newLi);
 };
+
+// function deleteItem(e) {
+//     e.preventDefault();
+
+//     const deleteData = {
+//         item: e.target.item.value,
+//         price: e.target.price.value
+//     };
+
+//     const options = {
+//         method: 'DELETE',
+//         body: JSON.stringify(deleteData)
+//     };
+
+//     fetch('http://localhost:3000/shoppingList/:id', options)
+//         .then(r => r.json()) // As this is a HTTP response, we want to extract JSON body content from the response
+//         .then(removeItem) 
+//         .catch(console.warn)        
+// }
+
+// function removeItem(delete){
+//     const lastItem = document.querySelector('li').lastElementChild;
+//     lastItem.remove();
+// }
